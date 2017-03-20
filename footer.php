c6e8a95e329d56f1a52d7ff2c190144d43ec8e54
Willis Allstead <willallstead@icloud.com>
2017-03-20 15:16:14 -0700

    first commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package BF_FUTURETASTIC
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bf_futuretastic' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'bf_futuretastic' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'bf_futuretastic' ), 'bf_futuretastic', '<a href="https://automattic.com/" rel="designer">Underscores.me</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
