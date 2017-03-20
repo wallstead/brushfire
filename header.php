c6dee8661a6be1089a1f339a22bf36f356beedcc
Willis Allstead <willallstead@icloud.com>
2017-03-20 16:27:53 -0700

    Added dynamic links to nav

--- header.php
+++ header.php
@@ -24,11 +24,12 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'bf_futuretastic' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="navbar">
+		<nav class="navbar" role="navigation">
 			<section class="navbar-primary">
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="navbar-brand mr-10" rel="home"><img class="logo" src="http://unrbrushfire.org/wp-content/uploads/2017/03/logo.png" alt="Brushfire "></a>
 			</section>
 			<section class="navbar-section">
+				<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
 				<a href="#" class="btn btn-link">About Us</a>
 				<a href="https://github.com/picturepan2/spectre" class="btn btn-link">Artist Spotlight</a>
 				<a href="https://github.com/picturepan2/spectre" class="btn btn-link">Contact</a>
@@ -36,12 +37,10 @@
 			<section class="navbar-section">
 				<button class="btn btn-primary">Submit</button>
 			</section>
-		</div>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'bf_futuretastic' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
+		</nav>
+
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
