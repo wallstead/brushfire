970ca6fd2aad49c549f144a231faf6104b11e517
Willis Allstead <willallstead@icloud.com>
2017-03-20 18:29:01 -0700

    Added typekit credentials

--- header.php
+++ header.php
@@ -16,6 +16,17 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
+<script> /* this will stop the page from showing until text is loaded */
+  (function(d) {
+    var config = {
+      kitId: 'eok4egk',
+      scriptTimeout: 3000,
+      async: true
+    },
+    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+  })(document);
+</script>
+
 <?php wp_head(); ?>
 </head>
 
