cfc0b1f83af5901e3bba0ad11f207d2bd4894c4c
Willis Allstead <willallstead@icloud.com>
2017-03-20 17:02:17 -0700

    moving submit button around

--- header.php
+++ header.php
@@ -23,15 +23,15 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'bf_futuretastic' ); ?></a>
 
-	<nav class="navbar" role="navigation">
-		<section class="navbar-primary">
+	<nav id="navbar" role="navigation">
+		<section>
 			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="navbar-brand mr-10" rel="home"><img class="logo" src="http://unrbrushfire.org/wp-content/uploads/2017/03/logo.png" alt="Brushfire "></a>
 		</section>
-		<section class="navbar-section">
+		<section>
 			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
 		</section>
-		<section class="navbar-section">
-			<button class="btn btn-primary">Submit</button>
+		<section class="navbar-right">
+			<button class="btn btn-primary">Submit Your Work</button>
 		</section>
 
 		<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'bf_futuretastic' ); ?></button>
