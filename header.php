9c18d55361692db96dfac5d5edd96334f90bcc17
Willis Allstead <willallstead@icloud.com>
2017-03-20 17:08:21 -0700

    added a header container

--- header.php
+++ header.php
@@ -22,19 +22,21 @@
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'bf_futuretastic' ); ?></a>
+	<div id="header-container">
+		<nav id="navbar" role="navigation">
+			<section>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="navbar-brand mr-10" rel="home"><img class="logo" src="http://unrbrushfire.org/wp-content/uploads/2017/03/logo.png" alt="Brushfire "></a>
+			</section>
+			<section>
+				<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
+			</section>
+			<section class="navbar-right">
+				<button class="btn btn-primary">Submit Your Work</button>
+			</section>
 
-	<nav id="navbar" role="navigation">
-		<section>
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="navbar-brand mr-10" rel="home"><img class="logo" src="http://unrbrushfire.org/wp-content/uploads/2017/03/logo.png" alt="Brushfire "></a>
-		</section>
-		<section>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
-		</section>
-		<section class="navbar-right">
-			<button class="btn btn-primary">Submit Your Work</button>
-		</section>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'bf_futuretastic' ); ?></button>
+		</nav>
+	</div>
 
-		<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'bf_futuretastic' ); ?></button>
-	</nav>
 
 	<div id="content" class="site-content">
