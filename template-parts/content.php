769fee8ba3aa8b91dcfeaf6f1c5fd6b83ae2a1b7
Willis Allstead <willallstead@icloud.com>
2017-04-12 15:32:17 -0700

    closing a bracket

--- template-parts/content.php
+++ template-parts/content.php
@@ -34,6 +34,7 @@
 			    foreach ( $portraitImages as $portraitImage ) {
 			        echo '<img style="padding:5px" src="', esc_url( $portraitImage['full_url'] ), '"  alt="', esc_attr( $portraitImage['alt'] ), '">';
 			    }
+			}
 			// echo '<img style="padding:5px" src="', esc_url( $portraitImage['full_url'] ), '"  alt="', esc_attr( $portraitImage['alt'] ), '">';
 
 
