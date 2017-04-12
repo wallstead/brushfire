bf4bc4e424c0d60169147a5ca2d6d3b6e41c08d5
Willis Allstead <willallstead@icloud.com>
2017-04-12 15:31:29 -0700

    real port image

--- template-parts/content.php
+++ template-parts/content.php
@@ -29,8 +29,12 @@
 	<div class="entry-content">
 		<?php
 
-			$portraitImage = rwmb_meta( 'portrait_upload')[0];
-			echo '<img style="padding:5px" src="', esc_url( $portraitImage['full_url'] ), '"  alt="', esc_attr( $portraitImage['alt'] ), '">';
+			$portraitImages = rwmb_meta( 'portrait_upload');
+			if ( !empty( $portraitImages ) ) {
+			    foreach ( $portraitImages as $portraitImage ) {
+			        echo '<img style="padding:5px" src="', esc_url( $portraitImage['full_url'] ), '"  alt="', esc_attr( $portraitImage['alt'] ), '">';
+			    }
+			// echo '<img style="padding:5px" src="', esc_url( $portraitImage['full_url'] ), '"  alt="', esc_attr( $portraitImage['alt'] ), '">';
 
 
 			$images = rwmb_meta( 'image_upload'); // Since 4.8.0
