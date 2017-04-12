1fea2371382c6f05f47e2f368b93e97bef7a5e47
Willis Allstead <willallstead@icloud.com>
2017-04-12 15:40:23 -0700

    making artist bio p tag

--- template-parts/content.php
+++ template-parts/content.php
@@ -37,15 +37,14 @@
 			}
 
 
-
-
-
+			echo '<p class="artistbio">';
 			the_content( sprintf(
 				/* translators: %s: Name of current post. */
 				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'bf_futuretastic' ), array( 'span' => array( 'class' => array() ) ) ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			) );
-
+			echo '</p>';
+			
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bf_futuretastic' ),
 				'after'  => '</div>',
