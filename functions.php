05c1878d00866e4cc948b5613c3178a928c7319a
Willis Allstead <willallstead@icloud.com>
2017-04-12 14:55:10 -0700

    changing new way

--- functions.php
+++ functions.php
@@ -7,7 +7,6 @@
  * @package BF_FUTURETASTIC
  */
 
- include 'demo.php';
 
 if ( ! function_exists( 'bf_futuretastic_setup' ) ) :
 /**
@@ -72,6 +71,42 @@ function bf_futuretastic_setup() {
 }
 endif;
 add_action( 'after_setup_theme', 'bf_futuretastic_setup' );
+
+
+add_filter( 'rwmb_meta_boxes', 'your_prefix_meta_boxes' );
+function your_prefix_meta_boxes( $meta_boxes ) {
+    $meta_boxes[] = array(
+        'title'      => __( 'Test Meta Box', 'textdomain' ),
+        'post_types' => 'post',
+        'fields'     => array(
+            array(
+                'id'   => 'name',
+                'name' => __( 'Name', 'textdomain' ),
+                'type' => 'text',
+            ),
+            array(
+                'id'      => 'gender',
+                'name'    => __( 'Gender', 'textdomain' ),
+                'type'    => 'radio',
+                'options' => array(
+                    'm' => __( 'Male', 'textdomain' ),
+                    'f' => __( 'Female', 'textdomain' ),
+                ),
+            ),
+            array(
+                'id'   => 'email',
+                'name' => __( 'Email', 'textdomain' ),
+                'type' => 'email',
+            ),
+            array(
+                'id'   => 'bio',
+                'name' => __( 'Biography', 'textdomain' ),
+                'type' => 'textarea',
+            ),
+        ),
+    );
+    return $meta_boxes;
+}
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
