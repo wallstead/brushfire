1562a99deed1b731b2c73b3c26a158948a46ec27
Willis Allstead <willallstead@icloud.com>
2017-05-06 11:45:55 -0700

    added horizontal lines around date in posts

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -25,7 +25,7 @@ function bf_futuretastic_posted_on() {
 	);
 
 	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'bf_futuretastic' ),
+		esc_html_x( '%s', 'post date', 'bf_futuretastic' ),
 		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 	);
 
