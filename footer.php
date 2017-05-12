c44441e1ff6c0403efd7e99608e01ba45679c041
Willis Allstead <willallstead@icloud.com>
2017-05-12 12:18:51 -0700

    fitText inline

--- footer.php
+++ footer.php
@@ -23,21 +23,33 @@
 <?php wp_footer(); ?>
 
 <script>
-	// $(document).ready(function(){
-	// 	function triggerIsotope() {
-	// 		$('.artistart').isotope({
-	// 			// options
-	// 			itemSelector: '.art',
-	// 			layoutMode: 'fitRows',
-	// 			percentPosition: true
-	// 		});
-	// 	}
-	// });
+	/* fittext */
+	(function($) {
+	    $.fn.fitText = function(kompressor, options) {
+	        // Setup options
+	        var compressor = kompressor || 1,
+	            settings = $.extend({
+	                'minFontSize': Number.NEGATIVE_INFINITY,
+	                'maxFontSize': Number.POSITIVE_INFINITY
+	            }, options);
+	        return this.each(function() {
+	            // Store the object
+	            var $this = $(this);
+	            // Resizer() resizes items based on the object width divided by the compressor * 10
+	            var resizer = function() {
+	                $this.css('font-size', Math.max(Math.min($this.width() / (compressor * 10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+	            };
+	            // Call once to set.
+	            resizer();
+	            // Call on resize. Opera debounces their resize by default.
+	            $(window).on('resize.fittext orientationchange.fittext', resizer);
+	        });
+	    };
+	})(jQuery);
 
 	$(document).ready(function(){
 		$('.artistart').imagesLoaded( function() {
 			$('.artistart').isotope({
-				// options
 				itemSelector: '.art',
 				layoutMode: 'fitRows',
 				percentPosition: true
@@ -46,8 +58,6 @@
 
 		$(".artist-name").fitText();
 	});
-
-
 </script>
 
 </body>
