445ea7079c2d2228e48826dd015f87caa4554172
Willis Allstead <willallstead@icloud.com>
2017-05-12 12:27:31 -0700

    trying fittext one last time with white-space:nowrap;

--- footer.php
+++ footer.php
@@ -24,28 +24,28 @@
 
 <script>
 	/* fittext */
-	// (function($) {
-	//     $.fn.fitText = function(kompressor, options) {
-	//         // Setup options
-	//         var compressor = kompressor || 1,
-	//             settings = $.extend({
-	//                 'minFontSize': Number.NEGATIVE_INFINITY,
-	//                 'maxFontSize': Number.POSITIVE_INFINITY
-	//             }, options);
-	//         return this.each(function() {
-	//             // Store the object
-	//             var $this = $(this);
-	//             // Resizer() resizes items based on the object width divided by the compressor * 10
-	//             var resizer = function() {
-	//                 $this.css('font-size', Math.max(Math.min($this.width() / (compressor * 10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
-	//             };
-	//             // Call once to set.
-	//             resizer();
-	//             // Call on resize. Opera debounces their resize by default.
-	//             $(window).on('resize.fittext orientationchange.fittext', resizer);
-	//         });
-	//     };
-	// })(jQuery);
+	(function($) {
+	    $.fn.fitText = function(kompressor, options) {
+	        // Setup options
+	        var compressor = kompressor || 1,
+	            settings = $.extend({
+	                'minFontSize': Number.NEGATIVE_INFINITY,
+	                'maxFontSize': Number.POSITIVE_INFINITY
+	            }, options);
+	        return this.each(function() {
+	            // Store the object
+	            var $this = $(this);
+	            // Resizer() resizes items based on the object width divided by the compressor * 10
+	            var resizer = function() {
+	                $this.css('font-size', Math.max(Math.min($this.width() / (compressor * 10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+	            };
+	            // Call once to set.
+	            resizer();
+	            // Call on resize. Opera debounces their resize by default.
+	            $(window).on('resize.fittext orientationchange.fittext', resizer);
+	        });
+	    };
+	})(jQuery);
 
 	$(document).ready(function(){
 		$('.artistart').imagesLoaded( function() {
@@ -56,7 +56,7 @@
 			});
 		});
 
-		// $(".artist-name").fitText();
+		$(".artist-name").fitText();
 	});
 </script>
 
