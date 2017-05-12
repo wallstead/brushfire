53c594c5a92b638f350b7b12d2e32d1bc573254f
Willis Allstead <willallstead@icloud.com>
2017-05-12 13:20:06 -0700

    removing fittext last time

--- footer.php
+++ footer.php
@@ -23,30 +23,6 @@
 <?php wp_footer(); ?>
 
 <script>
-	/* fittext */
-
-	(function($) {
-	    $.fn.fitText = function(kompressor, options) {
-	        // Setup options
-	        var compressor = kompressor || 1,
-	            settings = $.extend({
-	                'minFontSize': Number.NEGATIVE_INFINITY,
-	                'maxFontSize': Number.POSITIVE_INFINITY
-	            }, options);
-	        return this.each(function() {
-	            // Store the object
-	            var $this = $(this);
-	            // Resizer() resizes items based on the object width divided by the compressor * 10
-	            var resizer = function() {
-	                $this.css('font-size', Math.max(Math.min($this.width() / (compressor * 10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
-	            };
-	            // Call once to set.
-	            resizer();
-	            // Call on resize. Opera debounces their resize by default.
-	            $(window).on('resize.fittext orientationchange.fittext', resizer);
-	        });
-	    };
-	})(jQuery);
 
 	$('.artistbio').flowtype({
 		minimum   : 500,
@@ -63,8 +39,6 @@
 				percentPosition: true
 			});
 		});
-
-		$(".artist-name").fitText();
 	});
 </script>
 
