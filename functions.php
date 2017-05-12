65680b3541089abc46f42d0ded11213ec426dd09
Willis Allstead <willallstead@icloud.com>
2017-05-12 16:13:29 -0700

    adding svg support in media library

--- functions.php
+++ functions.php
@@ -197,6 +197,17 @@ function bf_futuretastic_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'bf_futuretastic_scripts' );
 
+function add_file_types_to_uploads($file_types){
+
+    $new_filetypes = array();
+    $new_filetypes['svg'] = 'image/svg+xml';
+    $file_types = array_merge($file_types, $new_filetypes );
+
+    return $file_types;
+}
+
+add_action('upload_mimes', 'add_file_types_to_uploads');
+
 /**
  * Implement the Custom Header feature.
  */
