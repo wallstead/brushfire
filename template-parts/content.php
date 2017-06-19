389731c06f0998825785a2c31375585737d5b227
Willis Allstead <willallstead@icloud.com>
2017-06-19 16:13:10 -0700

    added tags

--- template-parts/content.php
+++ template-parts/content.php
@@ -77,10 +77,10 @@
 			$artWritingTitle = rwmb_meta( 'writing_media_title');
 			echo $artWritingTitle;
 			echo '</h4>';
-			// echo '<p>';
+			echo '<p>';
 			// $artWriting = rwmb_meta( 'writing_media');
 			// echo $artWriting;
-			// echo '</p></div>';
+			echo '</p></div>';
 			echo '</div>';
 
 			wp_link_pages( array(
