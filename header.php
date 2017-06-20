be64c690e3b74143370a75ae8a3bebbba4ac91ae
Willis Allstead <willallstead@icloud.com>
2017-06-20 11:49:41 -0700

    testing longer fadein

--- header.php
+++ header.php
@@ -98,7 +98,7 @@
             // }
 
             $('.recent-journal').each(function(i, obj) {
-                $(this).fadeIn( 500*i, function() {
+                $(this).fadeIn( 1000, function() {
                     console.log("done.");
                 });
             });
