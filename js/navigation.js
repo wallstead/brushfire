bad9f748f9cf8e55cf58a571da4b14422f24ed7c
Willis Allstead <willallstead@icloud.com>
2017-06-23 11:25:51 -0700

    trying slidetoggle for nav

--- js/navigation.js
+++ js/navigation.js
@@ -2,18 +2,16 @@ $(document).ready(function(){
 	var navOpen = false;
 
     $("#navburger").click(function(){
-
 		if (navOpen) {
 			/* close nav */
 			$(this).removeClass("is-active");
-			$(".navlinks").css('height', '0');
 			navOpen = false;
 		} else {
 			/* open nav */
 			$(this).addClass("is-active");
-			$(".navlinks").css('height', 'auto');
 			navOpen = true;
 		}
-
+		$(".navlinks").slideToggle(1000, 'swing');
+		
     });
 });
