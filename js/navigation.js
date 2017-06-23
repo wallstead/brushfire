5c0164d9743de3fcbe2b2c79b2960dedee443845
Willis Allstead <willallstead@icloud.com>
2017-06-23 11:21:00 -0700

    added logic to toggle navburger

--- js/navigation.js
+++ js/navigation.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
+	var navOpen = false;
+
     $("#navburger").click(function(){
-        $(this).toggleClass("is-active");
-		$(".navlinks").css('height', 'auto');
+
+		if (navOpen) {
+			/* close nav */
+			$(this).removeClass("is-active");
+			$(".navlinks").css('height', '0');
+			navOpen = false;
+		} else {
+			/* open nav */
+			$(this).addClass("is-active");
+			$(".navlinks").css('height', 'auto');
+			navOpen = true;
+		}
+
     });
 });
