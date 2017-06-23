a212aea353017f05b9f52a524175e446bb1590c9
Willis Allstead <willallstead@icloud.com>
2017-06-23 11:30:42 -0700

    added swing to navlinks

--- js/navigation.js
+++ js/navigation.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 			$(this).addClass("is-active");
 			navOpen = true;
 		}
-		$(".navlinks").slideToggle(350, 'swing');
+		$(".navlinks").slideToggle(300, 'swing');
 
     });
 });
