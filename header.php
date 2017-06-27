1d0554721892affb75a7507db24df450b88281e0
Willis Allstead <willallstead@icloud.com>
2017-06-27 11:32:45 -0700

    adding loading animation

--- header.php
+++ header.php
@@ -95,6 +95,17 @@
                         }
                     }
                 ?>
+                <!-- loading animation -->
+                <div class="la-ball-spin-clockwise la-2x">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
             </div>
     	</div>
 
