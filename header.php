833010d60858d3b90f9065a834b9dd6c0f004cc3
Willis Allstead <willallstead@icloud.com>
2017-06-27 11:59:33 -0700

    fixing jquery

--- header.php
+++ header.php
@@ -115,7 +115,7 @@
             $('.loading-header').fadeOut( "slow", function() {
                 $('.loading-header').css("display", "none");
                 $('.recent-journal').each(function(i, obj) {
-                    $(this).animate({opacity:1}, 200*i, function() {
+                    obj.animate({opacity:1}, 200*i, function() {
 
                     });
                 });
